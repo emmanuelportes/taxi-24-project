7997eb30ec9609788739a1508e1b222632399370
Emmanuel <mportesz.08@gmail.com>
2023-01-15 22:30:56 -0400

    modified files

--- drivers-management/src/main.ts
+++ drivers-management/src/main.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-dotenv.config({path: './src/configure.env'});
+dotenv.config({path: './configure.env'});
 import  express from "express";
 import morgan from "morgan";
 import { router } from "./routes/drivers-routes";
